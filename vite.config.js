31c12cf97f0f76df4ba3f8a91711248d522dc312
daneriulisesjulian <ejemploweb26@gmail.com>
2026-05-06 14:42:35 -0300

    Eliminando node_modules para que Vercel pueda instalar

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   preview: {
     allowedHosts: true
+  },
+  server: {
+    host: true // Esto también ayuda a que se vea en red local
   }
 })
